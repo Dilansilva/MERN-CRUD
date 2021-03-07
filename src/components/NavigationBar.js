0660a261b1efbdda9db5381018f6ae43047c83fe
Dilan Silva <sdilansilva@gmail.com>
2021-03-07 21:55:37 +0530

    Navigation bar is done

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -2,14 +2,21 @@ import React from 'react';
 
 import {Button,//import bootstrap
         Form,
-        Jumbotron} from 'react-bootstrap';
+        Nav,
+        Navbar,
+        FormControl} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const NavigationBar = () => {
     return(
-        <div>
-            <h1>Navigation bar</h1>
-        </div>
+        <>
+            <Navbar bg="primary" variant="dark">
+                <Navbar.Brand href="#home">Create</Navbar.Brand>
+                <Navbar.Brand href="#home">Read</Navbar.Brand>
+                <Navbar.Brand href="#home">Update</Navbar.Brand>
+                <Navbar.Brand href="#home">Delete</Navbar.Brand>
+            </Navbar>
+        </>
     );
 }
 
