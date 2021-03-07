e9a04cfa8a6a098e0026c8c8c4e36c83c1193c12
Dilan Silva <sdilansilva@gmail.com>
2021-03-07 21:37:46 +0530

    Resolved some bootstrap

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Button,
-        Form,
-        } from 'react-bootstrap';
+
+import {Button,//import bootstrap
+        Form} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Create = () => {
     return(
