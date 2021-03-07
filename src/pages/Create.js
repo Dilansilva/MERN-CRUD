ba6bf650073cc74b2cbfba731bea41d8e76a1577
Dilan Silva <sdilansilva@gmail.com>
2021-03-07 21:41:00 +0530

    Create page UI is done

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,12 +1,13 @@
 import React from 'react';
 
 import {Button,//import bootstrap
-        Form} from 'react-bootstrap';
+        Form,
+        Jumbotron} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Create = () => {
     return(
-        <div>
+        <Jumbotron>
             <Form>
                 <Form.Group controlId="exampleForm.ControlInput1">
                     <Form.Label>Enter Heading here</Form.Label>
@@ -15,14 +16,14 @@ const Create = () => {
 
                 <Form.Group controlId="exampleForm.ControlTextarea1">
                     <Form.Label>Example textarea</Form.Label>
-                        <Form.Control as="textarea" rows={3} />
+                        <Form.Control as="textarea" rows={12} />
                 </Form.Group>
 
                 <Button variant="primary" type="submit">
                     Add Article
                 </Button>
             </Form>
-        </div>
+        </Jumbotron>
     );
 }
 
