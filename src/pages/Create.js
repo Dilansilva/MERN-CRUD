a418342eec88501f225c1e8378c2f7f83656a20a
Dilan Silva <sdilansilva@gmail.com>
2021-03-07 21:31:02 +0530

    Added bootstrap and make the form for create operation

--- src/pages/Create.js
+++ src/pages/Create.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import {Button,
+        Form,
+        } from 'react-bootstrap';
 
 const Create = () => {
     return(
         <div>
-            <h1>Create Page!</h1>
+            <Form>
+                <Form.Group controlId="exampleForm.ControlInput1">
+                    <Form.Label>Enter Heading here</Form.Label>
+                        <Form.Control type="text" placeholder="Heading" />
+                </Form.Group>
+
+                <Form.Group controlId="exampleForm.ControlTextarea1">
+                    <Form.Label>Example textarea</Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                </Form.Group>
+
+                <Button variant="primary" type="submit">
+                    Add Article
+                </Button>
+            </Form>
         </div>
     );
 }
