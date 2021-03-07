f43a4cfe627da7ba0a5307cc48bbd13ba909cd95
Dilan Silva <sdilansilva@gmail.com>
2021-03-07 21:49:05 +0530

    Navigation bar is created

---
+++ src/components/NavigationBar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import {Button,//import bootstrap
+        Form,
+        Jumbotron} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavigationBar = () => {
+    return(
+        <div>
+            <h1>Navigation bar</h1>
+        </div>
+    );
+}
+
+export default NavigationBar;
--- src/pages/Create.js
+++ src/pages/Create.js
@@ -5,25 +5,30 @@ import {Button,//import bootstrap
         Jumbotron} from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+import NavigationBar from '../components/NavigationBar';//import navigation bar
+
 const Create = () => {
     return(
+        <>
+            <NavigationBar/>
         <Jumbotron>
-            <Form>
-                <Form.Group controlId="exampleForm.ControlInput1">
-                    <Form.Label>Enter Heading here</Form.Label>
-                        <Form.Control type="text" placeholder="Heading" />
-                </Form.Group>
+        <Form>
+            <Form.Group controlId="exampleForm.ControlInput1">
+                <Form.Label>Enter Heading here</Form.Label>
+                    <Form.Control type="text" placeholder="Heading" />
+            </Form.Group>
 
-                <Form.Group controlId="exampleForm.ControlTextarea1">
-                    <Form.Label>Example textarea</Form.Label>
-                        <Form.Control as="textarea" rows={12} />
-                </Form.Group>
+            <Form.Group controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Example textarea</Form.Label>
+                    <Form.Control placeholder="Article Body is here" as="textarea" rows={11} />
+            </Form.Group>
 
-                <Button variant="primary" type="submit">
-                    Add Article
-                </Button>
-            </Form>
-        </Jumbotron>
+            <Button variant="primary" type="submit">
+                Add Article
+            </Button>
+        </Form>
+    </Jumbotron>
+        </>
     );
 }
 
