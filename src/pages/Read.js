cd6b8498489b3f98258a0a3c2cf252d4fc8fb386
Dilan Silva <sdilansilva@gmail.com>
2021-03-07 22:51:13 +0530

    State management done in read page

--- src/pages/Read.js
+++ src/pages/Read.js
@@ -1,14 +1,14 @@
-import React, { useState } from 'react';
-
-import {Button,//import bootstrap
-        Card,
-        Jumbotron} from 'react-bootstrap';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { useState,useEffect } from 'react';
 
 import NavigationBar from '../components/NavigationBar';//import navigation bar
 import Cards from '../components/Cards';//impotr card component
 
 const Read = () => {
+   const [article,setArticle] = useState('')//state for article
+   useEffect(() => {
+        //fetch api here
+   },[]);
+
     return(
         <>
             <NavigationBar/>
