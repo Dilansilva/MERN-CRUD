3e78d4d9b36f00bcf289c39ed41118faf4e0be71
Dilan Silva <sdilansilva@gmail.com>
2021-03-08 11:36:24 +0530

    State management done in Update page

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -4,6 +4,12 @@ import NavigationBar from '../components/NavigationBar';//import navigation bar
 import UpdateCard from '../components/UpdateCard';//import component for update
 
 const Update = () => {
+
+    const [Article,setArticle] = useState('');//state for article retreive
+
+    const [updateHeader,setupdateHeader] = useState('');//state for article header
+    const [updateBody,setupdateBody] = useState('');//state for article body
+    
     return(
         <div>
             <NavigationBar/>
