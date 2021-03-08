c4f0b8df0c5b3dd6e6614f1df74578b6a27ab03e
Dilan Silva <sdilansilva@gmail.com>
2021-03-08 12:29:20 +0530

    State management done in Delete page

--- src/pages/Delete.js
+++ src/pages/Delete.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
 
 export default class Delete extends Component {
+    constructor(props){
+        super(props);
+        this.state = [
+            
+        ];
+    }
     render() {
         return (
             <div>
