178b39f033b1f559e94acfd832746c35f36d6e38
Dilan Silva <sdilansilva@gmail.com>
2021-03-08 21:37:48 +0530

    Routing is done in the application

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import React from 'react';
+
+import {BrowserRouter as Router, 
+  Route, 
+    Switch  } from "react-router-dom";//essentials forreact-router
+
 import Create from './pages/Create';//import create page
 import Read from './pages/Read';//import Read page
 import Update from './pages/Update';//import Update page
@@ -5,9 +11,27 @@ import Delete from './pages/Delete';;//import Delete page
 
 function App() {
   return (
-    <div>
-      <Delete/>
-    </div>
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Create/>
+          </Route>
+
+          <Route path="/read">
+            <Read/>
+          </Route>
+
+          <Route path="/update">
+            <Update/>
+          </Route>
+
+          <Route path="/delete">
+            <Delete/>
+          </Route>  
+    </Switch>
+  </div>
+    </Router>
   );
 }
 
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -8,10 +8,10 @@ const NavigationBar = () => {
     return(
         <>
             <Navbar bg="primary" variant="dark">
-                <Navbar.Brand href="#home">Create</Navbar.Brand>
-                <Navbar.Brand href="#home">Read</Navbar.Brand>
-                <Navbar.Brand href="#home">Update</Navbar.Brand>
-                <Navbar.Brand href="#home">Delete</Navbar.Brand>
+                <Navbar.Brand href="/">Create</Navbar.Brand>
+                <Navbar.Brand href="/read">Read</Navbar.Brand>
+                <Navbar.Brand href="/update">Update</Navbar.Brand>
+                <Navbar.Brand href="/delete">Delete</Navbar.Brand>
             </Navbar>
         </>
     );
