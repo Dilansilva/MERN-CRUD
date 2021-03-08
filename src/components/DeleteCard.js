368caef43f3af6609259d7b8007a019c3d5ce5d9
Dilan Silva <sdilansilva@gmail.com>
2021-03-08 12:32:14 +0530

    Delete card is done

---
+++ src/components/DeleteCard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DeleteCard extends Component {
+    render() {
+        return (
+            <div>
+                <h1>jbjbj</h1>
+            </div>
+        )
+    }
+}
--- src/pages/Delete.js
+++ src/pages/Delete.js
@@ -1,16 +1,17 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import DeleteCard from '../components/DeleteCard';//import delete card
 
 export default class Delete extends Component {
     constructor(props){
         super(props);
         this.state = [
-            
+
         ];
     }
     render() {
         return (
             <div>
-                <h1>bnjbjb</h1>
+                <DeleteCard/>
             </div>
         )
     }
