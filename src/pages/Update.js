518da259cfeeae0a6b124bfdff94fde70e1ef4ac
Dilan Silva <sdilansilva@gmail.com>
2021-03-08 11:39:19 +0530

    convert into class based components Update page

--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,25 +1,15 @@
-import React, { useState,useEffect } from 'react';
-
 import NavigationBar from '../components/NavigationBar';//import navigation bar
 import UpdateCard from '../components/UpdateCard';//import component for update
 
-const Update = () => {
+import React, { Component } from 'react'
 
-    const [Article,setArticle] = useState('');//state for article retreive
-
-    const [updateHeader,setupdateHeader] = useState('');//state for article header
-    const [updateBody,setupdateBody] = useState('');//state for article body
-    
-    return(
-        <div>
-            <NavigationBar/>
-            <UpdateCard
-                defaultValueHeader="knknk"
-                defaultValueBody="jknknkn"
-                
-            />
-        </div>
-    );
+export default class Update extends Component {
+    render() {
+        return (
+            <div>
+                <NavigationBar/>
+                <UpdateCard/>
+            </div>
+        )
+    }
 }
-
-export default Update;
